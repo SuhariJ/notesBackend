90f0a2fce2daf847d6bf6c8621c176856e236485
SuhariJ <suhari.suhonen@gmail.com>
2025-02-28 15:17:46 +0200

    nyt päivittää automaatttisesti

--- osa3/index.js
+++ osa3/index.js
@@ -11,6 +11,7 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({
     error: "Unkonwn endpoint"
